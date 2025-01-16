4a760fbc557236ad431ce0b9dd2b6efd2e4670a9
Med Yacine Khouini <khouini.medyacine@gmail.com>
2025-01-16 07:43:39 +0100

    reverse array, recursive way

--- 7_Reverse_Array/main.cpp
+++ 7_Reverse_Array/main.cpp
@@ -4,6 +4,14 @@
 
 #include "main.h"
 
+void displayArray(const int* array, const int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl; // Add a newline for better readability
+}
+
+
 int* subArrayMethod(const int size, const int* inputArray) {
     int* resultArray = new int[size];
 
@@ -28,20 +36,28 @@ const int* optimisedMethod1(const int size, int* inputArray) {
 
 const int* optimisedMethod2(const int size, int* inputArray) {
     for (int i = 0; i < size / 2; i++) {
-        std::swap(inputArray[i], inputArray[size - i - 1]);
+        ranges::swap(inputArray[i], inputArray[size - i - 1]);
     }
     return inputArray;
 }
+
+const int* recursiveWay(int* inputArray, const int leftP, const int rightP) {
+   if (rightP < leftP) {
+       return inputArray;
+   }
+    ranges::swap(inputArray[leftP], inputArray[rightP]);
+    return recursiveWay(inputArray, leftP+1, rightP - 1);
+}
+
 
 int main() {
     int inputArray[] = {1, 2, 3, 4};
     constexpr int size = 4;
     // const int *resultArray = subArrayMethod(size, inputArray);
-    const int *resultArray = optimisedMethod2(size, inputArray);
+    // const int *resultArray = optimisedMethod2(size, inputArray);
+   const int *resultArray= recursiveWay(inputArray, 0, size - 1);
     // Print the result array
-    for (int i = 0; i < size; ++i) {
-        std::cout << resultArray[i] << " "; // Output the result
-    }
+    displayArray(resultArray, size);
 
     // delete[] resultArray; // Free the dynamically allocated memory
     return 0;
