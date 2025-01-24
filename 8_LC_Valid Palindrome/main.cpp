6b75d44b414165d5124e9fe05b66d7e64279cfbb
Med Yacine Khouini <khouini.medyacine@gmail.com>
2025-01-24 07:30:11 +0100

    validPalindrome recursive Way

--- 8_LC_Valid Palindrome/main.cpp
+++ 8_LC_Valid Palindrome/main.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 
 // iterative way
-bool validPalindrome1(const char *inputString, const int size) {
+bool iterativeWay(const char *inputString, const int size) {
     for (int i = 0; i < size / 2; i++) {
         const int leftP = i;
         const int rightP = size - i - 1;
@@ -20,9 +20,22 @@ bool validPalindrome1(const char *inputString, const int size) {
     return true;
 }
 
+bool recursiveWay(const char *inputString, const int leftP, const int rightP) {
+    if (rightP < leftP) {
+        return true;
+    }
+    const int leftValue = inputString[leftP];
+    const int rightValue = inputString[rightP];
+    if (leftValue != rightValue) {
+        return false;
+    }
+    return recursiveWay(inputString, leftP + 1, rightP - 1);
+}
+
 int main() {
     constexpr char inputString[] = "3223";
-    bool valid = validPalindrome1(inputString, 4);
+    // bool valid = iterativeWay(inputString, 4);
+    bool valid = recursiveWay(inputString, 0, 3);
     std::cout << "valid: " << valid;
     return 0;
 }
