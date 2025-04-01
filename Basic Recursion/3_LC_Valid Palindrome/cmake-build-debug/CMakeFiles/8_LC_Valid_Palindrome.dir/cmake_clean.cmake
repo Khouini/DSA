77771651c66c7aa8b479ce52098d797422f2aa13
khouini <khouini.medyacine@gmail.com>
2025-04-01 17:38:58 +0100

    refactor

---
+++ Basic Recursion/3_LC_Valid Palindrome/cmake-build-debug/CMakeFiles/8_LC_Valid_Palindrome.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_LC_Valid_Palindrome"
+  "8_LC_Valid_Palindrome.pdb"
+  "CMakeFiles/8_LC_Valid_Palindrome.dir/main.cpp.o"
+  "CMakeFiles/8_LC_Valid_Palindrome.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_LC_Valid_Palindrome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Basic Recursion/3_LC_Valid Palindrome/cmake-build-debug/CMakeFiles/8_LC_Valid_Palindrome.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for 8_LC_Valid_Palindrome.
+# This may be replaced when dependencies are built.
