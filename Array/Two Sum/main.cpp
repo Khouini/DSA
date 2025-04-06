d36ab3739215ea1fd980c0a80e3a60b42c9532fc
khouini <khouini.medyacine@gmail.com>
2025-04-06 01:24:57 +0100

    Two Sum greedy approach

--- Array/Two Sum/main.cpp
+++ Array/Two Sum/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 // class Solution {
 // public:
@@ -36,25 +37,52 @@ using namespace std;
 //         return {};
 //     }
 // };
+// class Solution {
+// public:
+//     std::vector<int> twoSum(std::vector<int> &nums, int target) {
+//         map<int, int> numsIndexes;
+//         // for (int i = 0; i < nums.size(); i++) {
+//         //     numsIndexes[nums[i]] = i;
+//         // }
+//         for (int i = 0; i < nums.size(); i++) {
+//             int calculatedTarget = target - nums[i];
+//             if (numsIndexes.contains(calculatedTarget)) {
+//                 int j = numsIndexes[calculatedTarget];
+//                 if (i == j) {
+//                     continue;
+//                 }
+//                 return {j, i};
+//             }
+//             numsIndexes[nums[i]] = i;
+//         }
+//         return {};
+//     }
+// };
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int> &nums, int target) {
-        map<int, int> numsIndexes;
-        // for (int i = 0; i < nums.size(); i++) {
-        //     numsIndexes[nums[i]] = i;
-        // }
+        // track values with its original index before sorting
+        vector<pair<int, int> > indexedNums;
         for (int i = 0; i < nums.size(); i++) {
-            int calculatedTarget = target - nums[i];
-            if (numsIndexes.contains(calculatedTarget)) {
-                int j = numsIndexes[calculatedTarget];
-                if (i == j) {
-                    continue;
-                }
-                return {j, i};
-            }
-            numsIndexes[nums[i]] = i;
+            indexedNums.push_back({nums[i], i});
         }
-        return {};
+        sort(indexedNums.begin(), indexedNums.end());
+        int leftP = 0;
+        int rightP = nums.size() - 1;
+        while (rightP > leftP) {
+            int leftValue = indexedNums[rightP].first;
+            int rightValue = indexedNums[leftP].first;
+            int sum = leftValue + rightValue;
+            if (sum > target) {
+                rightP--;
+            } else if (sum < target) {
+                leftP++;
+            } else {
+                // nums[rightP] + nums[leftP] == target
+                return {indexedNums[leftP].second, indexedNums[rightP].second};
+            }
+        }
+        return {-1, -1};
     }
 };
 
